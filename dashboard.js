e42a24b8b6140d44945f3a2df1f393124b697a8c
aysedo <coders2122@gmail.com>
2023-11-14 01:40:12 +0100

    new design

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,33 @@
-import {renderApp} from "./main.js"
+const app = document.getElementById("app");
+
+
+const renderNav = () => {
+    app.innerHTML += `
+        <div class="navbar">
+         <a href="#" class="lms">LMS</a>
+            <a href="#" class="home">Home</a>
+            <a href="#" class="teachers">Teachers</a>
+            <a href="#" class="students">Students</a>
+            <a href="#" class="user">User</a>
+            <div>
+              <i class="fa-regular fa-user"></i>
+            </div>
+        </div>
+
+        <h1 id="greeting">Welcome Mirijam</h1>
+        
+        <section id="section_one">
+            <div class="students-container">numbers of students</div>
+            <div class="teachers-container">numbers of teachers</div>
+            <div class="classes-container">numbers of classes</div>
+        </section>
+        
+        
+        `;
+};
+
+const renderApp = () => {
+    renderNav();
+
+};
+export { renderApp };
--- main.js
+++ main.js
@@ -1,34 +1,5 @@
-export function renderApp() {
-    renderHeader()
-    // renderContent(getProduct());
-    // renderFooter();
-}
+import {renderApp} from "./dashboard.js"
 
-export function renderHeader() {
-    const header = document.querySelector("app")
-    header.innerHTML =
-        `<nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">LMS</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Classes</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Teachers</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Students</a>
-                            </li>
-                    </div>`
-}
+
+renderApp ();
 
