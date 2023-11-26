d5631103d682d5ec7c8d2cc7282fe638926d4da4
aysedo <coders2122@gmail.com>
2023-11-27 00:04:30 +0100

    i added bootstrap again because i need it for my layouting

---
+++ students.js
@@ -0,0 +1,18 @@
+
+const students=[{
+    name:"Max",
+    surName:"Hermann",
+    average:" ",
+},
+{
+    name:"Alice",
+    surName:"Müller",
+    average:" ",
+},
+{
+    name:"Peter",
+    surName:"Pan",
+    average:" ",
+}
+
+]
