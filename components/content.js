7fc6422066f7a4d341756c467b7ccbda35944d34
aysedo <coders2122@gmail.com>
2023-12-08 18:55:23 +0100

    card added into Teachers content

--- components/content.js
+++ components/content.js
@@ -82,11 +82,36 @@ export function renderContent(pPage) {
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" class="card-link">Students</a>
       <a href="#" class="card-link">Teachers</a>
-  </div>`
+  </div>
+    `
+    
   }
 
   else if (pPage === "teacher") {
-    contentContainer.innerHTML = `teachers`
+    contentContainer.innerHTML = `
+    <h1 class="m-5">Teachers</h1>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+        <div class="card ml-10" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Students</a>
+        <a href="#" class="card-link">Classes</a>
+      </div>
+    </div>
+    </div>
+    <div class="col">
+      <div class="card ml-10" style="width: 18rem;">
+      <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="card-link">Students</a>
+      <a href="#" class="card-link">Classes</a>
+  </div>`
   }
   else if (pPage === "student") {
     contentContainer.innerHTML = `students`
