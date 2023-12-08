65d01d6318f91cfb2a7ee415268e10a11afaf4a2
aysedo <coders2122@gmail.com>
2023-12-08 09:11:44 +0100

    content created

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { studentsData } from "./components./students.js";
 import { header } from "./components/header.js";
 import { render } from "./components./function.js";
-import { content } from "./content.js";
+import { content } from "./components/content.js";
 
 render('#app',header +content)
 
