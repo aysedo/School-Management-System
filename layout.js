8b72868c3da153b6fc0e1b48b1458d7cf899ee4f
aysedo <coders2122@gmail.com>
2023-12-08 08:26:23 +0100

    studentsData added

---
+++ layout.js
@@ -0,0 +1,4 @@
+export const layout=`
+<div class="container">
+<div id="content">Content</div>
+</div>`
--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-import { employeesData } from ".studentsComponent.js";
+import { studentsData } from ".studentsComponent.js";
 import { header } from "./components/header.js";
-import { layout } from "./components/layout.js";
 import { render } from "./function.js";
 
-render('#app',header+layout)
+render('#app',header +layout)
 
 
 
 
 
 
-document.querySelector("#employees-link").addEventListener("click",()=>{render("#content",employeesData.content)})
-document.querySelector("#hr-link").addEventListener("click",()=>{render("#content",employeesData.name)})
+document.querySelector("#employees-link").addEventListener("click",()=>{render("#content",studentsData.content)})
+document.querySelector("#hr-link").addEventListener("click",()=>{render("#content",teachersData.name)})
 document.querySelector("#settings-link").addEventListener("click",()=>{render("#content","settings")})
 
 
