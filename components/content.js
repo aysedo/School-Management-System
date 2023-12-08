11c1edda0cd6f337fd07d25f4b7eae0257a0dd0b
aysedo <coders2122@gmail.com>
2023-12-08 18:44:23 +0100

    Classes link added two card with student aand teachers link

--- components/content.js
+++ components/content.js
@@ -60,15 +60,28 @@ export function renderContent(pPage) {
 
   else if (pPage === "class") {
     contentContainer.innerHTML = `
-    <h1>Welcome Mirijam</h1>
-      <div class="card" style="width: 18rem;">
+    <h1 class="m-5">Classes</h1>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+        <div class="card ml-10" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Students</a>
+        <a href="#" class="card-link">Teachers</a>
+      </div>
+    </div>
+    </div>
+    <div class="col">
+      <div class="card ml-10" style="width: 18rem;">
       <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
-    </div>
+      <a href="#" class="card-link">Students</a>
+      <a href="#" class="card-link">Teachers</a>
   </div>`
   }
 
