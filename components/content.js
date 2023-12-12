5b4ffc59b02bf92999bad8627d1254be42323fb2
aysedo <coders2122@gmail.com>
2023-12-12 23:32:10 +0100

    data-toggle added to button add new teacher

--- components/content.js
+++ components/content.js
@@ -25,72 +25,76 @@ export function renderContent(pPage) {
           </div>
         </div>
     `
-   }else if (pPage === "class") {
-      contentContainer.innerHTML = `
+  } else if (pPage === "class") {
+    contentContainer.innerHTML = `
     <h1 class="m-5">Classes</h1>
     <div class="container text-center">
       <div class="row">
       ${classesData.map(classcontent => {
-        return `
+      return `
             <div class="col-4">
               <div class="card">
-                  <i class="bi bi-pencil-square"></i>
-                  <i class="bi bi-x-circle"></i>
+                  <a href="#" class="card-link" ><i class="bi bi-pencil-square"></i></a>
+                  <a href="#" class="card-link" ><i class="bi bi-x-circle"></i></a>
                   <div class="card-body">
                       <h5 class="card-title">${classcontent.class}</h5>
                       <h6>${classcontent.teacher}</h6>
-                      <p class="card-text">Bazı hızlı örnek metinler kartın içeriğini oluşturmak için buraya yerleştirilebilir.</p>
-                       <a href="#" class="card-link">Students</a>
+                      <p class="card-text">The classes in an IT school encompass various levels from beginners to advanced.</p>
+                       <a href="#" class="card-link" >Students</a>
                        <a href="#" class="card-link">Teachers</a>
                   </div>
               </div>
             </div>
         `
-      }).join(" ")
-        }
+    }).join(" ")
+      }
       </div>
-      <div class="row">
-        <div class="col">
-          <button id="add-students-btn">Add Class</button>
+        <div class="row">
+          <div class="col">
+            <button class="m-5">Add new Class</button>
+          </div>
         </div>
-      </div>
-    </div
+      </div
       `
-    }
+  }
 
-    else if (pPage === "teacher") {
-      contentContainer.innerHTML = `
+  else if (pPage === "teacher") {
+    contentContainer.innerHTML = `
     <h1 class="m-5">Teachers</h1>
     <div class="container text-center">
       <div class="row">
       ${teachersData.map(teacher => {
-        return `
+      return `
           <div class="col-4">
             <div class="card">
                 <i class="bi bi-pencil-square"></i>
                 <i class="bi bi-x-circle"></i>
                 <div class="card-body">
                     <h5 class="card-title">${teacher.name}   ${teacher.surName}</h5>
-                    <p class="card-text">An IT teacher is an education professional specialized in instructing information technology concepts. They possess in-depth knowledge of computer systems, software development, networks, and databases.</p>
-                    <a href="#" class="primary m-5">Students</a>
-                    <a href="#" class="primary">Class</a>
+                    <p class="card-text">An IT teacher is an education professional specialized in instructing information technology concepts.</p>
+                    <a href="#" class="card-link">Students</a>
+                    <a href="#" class="card-link">Class</a>
                 </div>
               </div>
           </div>`
-      }).join(" ")
-        }
+    }).join(" ")
+      }
       </div>
-    </div>  
-      `
-    }
+        <div class="row">
+          <div class="col">
+            <button class="m-5" data-toggle="modal" data-target="#addTeacherModal">Add new Teacher</button>
+          </div>
+        </div>
+      </div>`
+  }
 
-    else if (pPage === "student") {
+  else if (pPage === "student") {
 
-      contentContainer.innerHTML = `
+    contentContainer.innerHTML = `
     <h1 class="m-5">Students</h1>
     <div class="container text-center">
       <div class="row">${studentsData.map(student => {
-        return `
+      return `
                 <div class="col-4">
                   <div class="card">
                       <i class="bi bi-pencil-square"></i>
@@ -98,29 +102,29 @@ export function renderContent(pPage) {
                       <div class="card-body">
                           <h5 class="card-title">${student.name}  ${student.surName}</h5>
                           <h6>${student.class}</h6>
-                          <p class="card-text">Bazı hızlı örnek metinler kartın içeriğini oluşturmak için buraya yerleştirilebilir.</p>
+                          <p class="card-text">A cloud class focuses on teaching concepts related to cloud computing, covering topics such as virtualization, cloud infrastructure, deployment models, and cloud services.</p>
                           <a href="#" id="average-grade">Average Grade:4.9</a>
                       </div>
                   </div>
                 </div>
             `
-      }).join(" ")
+    }).join(" ")
 
 
 
-        }
+      }
       </div>
-      <div class="row">
-        <div class="col">
-          <button id="add-students-btn">Add Student</button>
+        <div class="row">
+          <div class="col">
+            <button class="m-5">Add new Student</button>
+          </div>
         </div>
-      </div>
-    </div>`
+      </div>`
 
-      document.querySelector('#add-students-btn').addEventListener('click', function () {
-        console.log('clicked')
-      })
-    }
-
-
+    document.querySelector('#add-students-btn').addEventListener('click', function () {
+     
+    })
   }
+
+
+}
--- components/function.js
+++ components/function.js
@@ -1,2 +1,4 @@
-
+const function addNEWStudent(){
+    
+}
 
