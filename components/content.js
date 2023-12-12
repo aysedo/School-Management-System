8617b4c7807888209a6ed2796ea62f05f0d83b95
aysedo <coders2122@gmail.com>
2023-12-12 20:42:08 +0100

    classes added to class content

--- components/content.js
+++ components/content.js
@@ -1,5 +1,6 @@
 import { teachersData } from "./data.js"
 import { studentsData } from "./data.js"
+import { classesData } from "./data.js"
 
 const contentContainer = document.querySelector('#content-container')
 
@@ -53,28 +54,31 @@ export function renderContent(pPage) {
     <h1 class="m-5">Classes</h1>
     <div class="container text-center">
       <div class="row">
-        <div class="col">
-        <div class="card ml-10" style="width: 18rem;">
-        <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="card-link">Students</a>
-        <a href="#" class="card-link">Teachers</a>
+      ${contentContainer.innerHTML += classesData.map(classcontent => {
+      return `
+            <div class="col-4">
+              <div class="card">
+                  <i class="bi bi-pencil-square"></i>
+                  <i class="bi bi-x-circle"></i>
+                  <div class="card-body">
+                      <h5 class="card-title">${classcontent.class}</h5>
+                      <h6>${classcontent.teacher}</h6>
+                      <p class="card-text">Bazı hızlı örnek metinler kartın içeriğini oluşturmak için buraya yerleştirilebilir.</p>
+                       <a href="#" class="card-link">Students</a>
+                       <a href="#" class="card-link">Teachers</a>
+                  </div>
+              </div>
+            </div>
+        `
+        }).join(" ")
+      }
       </div>
-    </div>
-    </div>
-    <div class="col">
-      <div class="card ml-10" style="width: 18rem;">
-      <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="card-link">Students</a>
-      <a href="#" class="card-link">Teachers</a>
-  </div>
-    `
-
+      <div class="row">
+        <div class="col">
+          <button>Add Student</button>
+        </div>
+      </div>
+    </div>`
   }
 
   else if (pPage === "teacher") {
